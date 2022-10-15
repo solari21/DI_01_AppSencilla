b03a8cdf40874156fa27d57b23d6a79b95e0392a
solari21 <solari21@gmail.com>
2022-10-15 19:18:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/holausuario/SaludoActivity.kt
@@ -0,0 +1,27 @@
+package com.example.holausuario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.View
+
+
+class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var txtSaludo : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        //Recuperamos la información pasada en el intent
+        val saludo = intent.getStringExtra("NOMBRE")
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.text = "Hola $saludo"
+
+    }
+}
